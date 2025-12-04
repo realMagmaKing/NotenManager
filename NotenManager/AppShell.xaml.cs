dff555cfbfbf456bfbc2732f0ddde7569caf2d8a
vmadmin <vmadmin@WIV11-VMWP>
2025-12-04 10:47:14 +0100

    Add project files.

---
+++ NotenManager/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NotenManager
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ NotenManager/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace NotenManager
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
